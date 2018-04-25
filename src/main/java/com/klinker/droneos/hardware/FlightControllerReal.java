4388623767764e83ad40a2294942bc2df5520dc3
Aaron Klinker <aaronklinker1@gmail.com>
2018-04-24 20:15:24 -0500

    Updated pwm

--- src/main/java/com/klinker/droneos/hardware/FlightControllerReal.java
+++ src/main/java/com/klinker/droneos/hardware/FlightControllerReal.java
@@ -3,29 +3,22 @@ package com.klinker.droneos.hardware;
 import java.io.IOException;
 import java.util.List;
 
-import com.pi4j.component.servo.ServoProvider;
 import com.klinker.droneos.utils.Log;
-import com.pi4j.component.servo.ServoDriver;
-import com.pi4j.component.servo.impl.RPIServoBlasterProvider;
+import com.pi4j.wiringpi.Gpio;
 
 public class FlightControllerReal extends FlightController {
 
-    private ServoDriver mThrust;
-
     FlightControllerReal(int strafeXPin, int strafeYPin, int anglePin, int liftPin) {
         super(strafeXPin, strafeYPin, anglePin, liftPin);
-        try {
-            ServoProvider servoProvider = new RPIServoBlasterProvider();
-            mThrust = servoProvider.getServoDriver(servoProvider.getDefinedServoPins().get(7));
-            Log.d("FC", "Thrust Pin: " + mThrust.getPin().getName());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Gpio.pinMode(7, Gpio.PWM_OUTPUT);
+        Gpio.pwmSetMode(Gpio.PWM_MODE_MS);
+        Gpio.pwmSetClock(384);
+        Gpio.pwmSetRange(1000);
     }
 
 	@Override
 	public void move(double strafeX, double strafeY, double angle, double lift) {
-		mThrust.setServoPulseWidth((int) (lift * 1000) + 1000);
+        Gpio.pwmWrite(7, 75);
     }
     
 }
